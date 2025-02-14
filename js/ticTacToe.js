315a607d4d9298eea03969631e733251982b39ab
edsonbustos <bustosedson@yahoo.com>
2025-02-14 08:37:17 -0600

    actualizacion2

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,17 +1,34 @@
-const celdas =document.getElementsByClassName("celdas")
-const reiniciarBtn = document.getElementById('reiniciar');
+const celdas = document.getElementsByClassName("celdas");
+const reiniciar = document.getElementById('reiniciar');
+const modoJugador = document.getElementById('modoJugador');
+const modoComputadora = document.getElementById('modoComputadora');
+const modoSeleccionado = document.getElementById('modoSeleccionado');
 
 let turno = "X"; 
-let pierde = false;  
+let pierde = false; 
+let juegoModo = "";
 
-for (let index = 0; index < celdas.length; index++) {
-    celdas[index].textContent = "";  
-    celdas[index].addEventListener("click", function () {
-        if (pierde || celdas[index].textContent !== "" || turno === "o") return;
-        verificarGanador();  
-        turno = "pierde";  
-        if (!pierde) {
-            computadora(); 
-        }
-    });
+modoJugador.addEventListener("click", function() {
+    juegoModo = "multijugador";
+    modoSeleccionado.textContent = "Jugador vs Jugador" ; // Muestra el modo seleccionado
+    iniciarJuego();
+});
+modoComputadora.addEventListener("click", function() {
+    juegoModo = "computadora";
+    modoSeleccionado.textContent = "Jugador vs Computadora";  // Muestra el modo seleccionado
+    iniciarJuego();
+});
+function iniciarJuego() {
+    pierde = false;
+    for (let index = 0; index < celdas.length; index++) {
+        celdas[index].textContent = "";  
+        celdas[index].addEventListener("click", function () {
+            if (pierde || celdas[index].textContent !== "") return; // Se detiene si la celda está llena
+            celdas[index].textContent = turno; 
+            verificarGanador();  
+  
+        });
+    }
 }
+
+
